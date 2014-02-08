26f1a3678301c330777f53696e1956556f30ed18
William Ersing <ersingw1@tcnj.edu>
2014-02-07 22:21:28 -0500

    data is now held in a hash (correctly)

--- scrape.rb
+++ scrape.rb
@@ -1,3 +1,17 @@
+=begin
+#---------------------------------------------------------------------
+
+scraper.rb
+written by: William Ersing
+
+This code scrapes country specific population data from Wikipedia.
+The purpose is to learn about web scraping, specifically with the
+Ruby language. Information is stored in a hash, and is displayed to 
+the user in HTML (this part is under construction)
+
+#---------------------------------------------------------------------
+=end
+
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
@@ -5,8 +19,23 @@ require 'open-uri'
 URL = "http://en.wikipedia.org/wiki/List_of_countries_by_population"
 doc = Nokogiri::HTML(open(URL))
 
-countries = doc.css('table.wikitable tr:nth-child(2)~tr td:nth-child(2) a:nth-child(2)')
-countries.each {|name| puts name['title']}
+countries = Array.new
+populations = Array.new
+country_list = {}
 
-populations = doc.css('table.wikitable tr:nth-child(2)~tr td:nth-child(3)') 
-populations.each {|num| puts num.text}
+doc.css('table.wikitable tr:nth-child(2)~tr').each do |item|
+    countries << item.at_css('td:nth-child(2) a:nth-child(2)')['title']
+    populations << item.at_css('td:nth-child(3)').text
+end
+
+@countries=countries
+@populations=populations
+
+(0..@countries.length - 1).each do |index|
+    country_list[@countries[index]] = @populations[index]
+end
+
+puts "Here is a list of populations by country\n\n"
+country_list.each do |key, value|
+    puts "#{key}:\t\t#{value}"
+end
