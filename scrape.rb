ee0fba8fe830fac157830563435418c559d12e43
William Ersing <ersingw1@tcnj.edu>
2014-02-10 13:22:22 -0500

    (scrape, UI, print data - all working) - TO DO: scrape/add religious data to project

--- scrape.rb
+++ scrape.rb
@@ -42,41 +42,28 @@ end
 #==========================================================================
 
 
-selection = ''
+selection = '' 
 puts "\n     \\\\\\\\\\\ POPULATIONS /////"
-while selection.casecmp('quit') != 0 do
+until selection == 'Quit' do
     puts "\n================================="
     puts "~Enter the name of a country\n~Type 'all' to show all countries\n~Type 'quit' to exit" 
     puts "=================================\n"
-    selection = gets.chomp
-    if selection.casecmp('quit') != 0
-	if selection.casecmp('all') == 0
+    selection = gets.chomp.split.map(&:capitalize).join(' ')
+    if selection != 'Quit' 
+	if selection == 'All' 
 	    longest_key = country_list.keys.max {|key1, key2| key1.length <=> key2.length}
-	    puts ''
+	    puts ''	# might not be necessary
 	    country_list.each do |key, value|
 		 printf("%-#{longest_key.length}s %s\n", key, value)
 	    end
 	else
-	    country_list.each do |key, value|
-		# working on comparing user input to keys to find a match
-		# so far this is fruitless - internet fails me too!
-		# HOW TO SEARCH/COMPARE KEY VALUES TO A STRING!?!?
 		#----------------------------------------
-		if country_list.key?(selection)
-		    puts "#{selection}: #{value}"
-		    break
-		else puts "#{key} is not in the list of countries"
-		end
-		#puts country_list.keys.to_s
-		#puts key.to_s
-		#----------------------------------------
-=begin
-		if selection.casecmp(key.to_s) == 0
-		    printf("%-#{key.length}s %s\n", key, value)
-		end
-=end
+	    if country_list.has_key?(selection)
+		puts "#{selection}: #{country_list[selection]}"
+	    else
+		puts "#{selection} was not found"
 	    end
-	    #puts "#{selection} is not in the list of countries"
+		#----------------------------------------
 	end
     end
 end
